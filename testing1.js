96cba3f4ee3f2ac929e55ac87fc976c0aa610c03
yusufkashim <110165409+yusufkashim@users.noreply.github.com>
2022-08-03 13:26:17 +0100

    Add files via upload

---
+++ testing1.js
@@ -0,0 +1,20 @@
+console.log("Hello my name is yusuf")
+
+let a=123432
+
+console.log(a)
+console.log(typeof(a))
+
+
+
+let c =" Yusuf Kashim" + 3000
+
+console.log(typeof(c))
+
+let required = true
+
+console.log(typeof(required))
+
+let f = a+c
+console.log(f)
+console.log(!required)
